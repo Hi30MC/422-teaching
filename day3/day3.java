40a71318b7b9e13bdfbb51df53d6450f49857eeb
HiHi3690 <duimstraash@gmail.com>
2025-02-14 15:10:47 -0500

    day3 init

---
+++ day3/day3.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+class day3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+
+
+    // return true if number is even
+    public static boolean isEven(int number) {
+        return false;
+    }
+
+    // sysout even numbers in given array
+    public static void printEvenNumbers(int[] numbers) {
+        
+    }
+
+    // return array of numbers that are even in given array
+    public static int[] getEvenNumbers(int[] numbers) {
+        return new int[0];
+    }
+
+    // return reversed arraylist of numbers
+    public static ArrayList<Integer> reverseArrayList(ArrayList<Integer> numbers) {
+        return new ArrayList<Integer>();
+    }
+
+    // return sum of all numbers in given array
+    public static int sumArray(int[] numbers) {
+        return 0;
+    }
+}
